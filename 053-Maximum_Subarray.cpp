20817e95e3f23487eb93ea9597fa734c8e399631
upTina <1226141677@qq.com>
2018-02-26 11:39:15 +0800

    Update 053-Maximum_Subarray.cpp

--- 053-Maximum_Subarray.cpp
+++ 053-Maximum_Subarray.cpp
@@ -15,5 +15,30 @@ public:
     }
 };
 //按道理这是个经典算法了，操作起来也非常简单，值得仔细思考。上面方法的时间复杂度为O(n)，还可以使用分治法，时间复杂度为O(nlogn)，如下：
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+		if (nums.size() == 0) return 0;
+		return findmax(nums, 0, nums.size() - 1);
+	}
+	int findmax(vector<int>& nums, int left, int right) {
+		if (left >= right) return nums[left];
+		int mid = (left + right) / 2;
+		int lmax = findmax(nums, left, mid - 1);
+		int rmax = findmax(nums, mid + 1, right);
+		int mmax = nums[mid], t = mmax;
+		for (int i = mid - 1; i >= left; i--) {
+			t += nums[i];
+			mmax = max(t, mmax);
+		}
+		t = mmax;
+		for (int i = mid + 1; i <= right; i++) {
+			t += nums[i];
+			mmax = max(t, mmax);
+		}
+		return max(lmax, max(rmax, mmax));
+	}
+};
 
+//max只能有2个参数
 
