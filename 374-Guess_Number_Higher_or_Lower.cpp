cc594b0d441851b5b92f3e562390de463d35b228
upTina <1226141677@qq.com>
2018-03-12 09:19:06 +0800

    Create 374-Guess_Number_Higher_or_Lower.cpp

---
+++ 374-Guess_Number_Higher_or_Lower.cpp
@@ -0,0 +1,25 @@
+int guess(int num);
+
+class Solution {
+public:
+    int guessNumber(int n) {
+        int low = 1;
+		int high = n;
+		int mid;
+        int flag = 0;
+		while (low <= high) {
+			mid = low+( high - low) / 2;
+            flag = guess(mid);
+            if (flag == 1) {
+				low = mid + 1;
+			}
+			else if(flag==-1) {
+				high = mid - 1;
+			}
+			 else return mid;
+			
+		}
+    }
+};
+//其他代码都不变，mid = (low+high)/ 2就会超时，而mid = low+( high - low) / 2就会ac。
+//以后求中点，认准mid=low+(high-low)/2
